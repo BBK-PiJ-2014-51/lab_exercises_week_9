01d761b96a9bd5f42b059c9127d7b875617409b5
unknown <cclayt02@CSPC045.dcs.bbk.ac.uk>
2014-11-24 20:07:11 +0000

    added ex2.2

--- Exercise2.java
+++ Exercise2.java
@@ -1,5 +1,18 @@
 class Exercise2{
 	public static void main(String[] args) {
-	
+		//ex2.1 tests
+				System.out.println("Ex2.1 tests");
+				IntSet tree = new TreeIntSet(50);
+				tree.add(25);
+				tree.add(26);
+				tree.add(77);
+				tree.add(91);
+				tree.add(13);
+				tree.add(44);
+				tree.add(56);
+				tree.add(77);
+				tree.containsVerbose(26);
+				tree.containsVerbose(77);
+				System.out.println("");
 	}
 }
---
+++ TreeIntSet.java
@@ -0,0 +1,72 @@
+class TreeIntSet implements IntSet{
+	int value;
+	TreeIntSet left;
+	TreeIntSet right;
+	
+	public TreeIntSet(int value){
+		this.value = value;
+		left = null;
+		right = null;
+	}
+	
+	@Override
+	public boolean add(int num) {
+		if (num == this.value) return false;
+		if (num > this.value) {
+			if (right == null) {
+				right = new TreeIntSet(num);
+				return true;
+			} else {
+				right.add(num);
+			}
+		} else {
+			if (left == null) {
+				left = new TreeIntSet(num);
+				return true;
+			} else {
+				left.add(num);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean contains(int num) {
+		if (num == this.value) {
+			return true;
+		} else if (num > this.value) {
+			if (right == null) {
+				return false;
+			} else {
+				return right.contains(num);
+			}
+		} else {
+			if (left == null) {
+				return false;
+			} else {
+				return left.contains(num);
+			}
+		}
+	}
+
+	@Override
+	public boolean containsVerbose(int num) {
+		System.out.println("Checking current num: " + value + " for match against: " + num);
+		if (num == this.value) {
+			return true;
+		} else if (num > this.value) {
+			if (right == null) {
+				return false;
+			} else {
+				return right.containsVerbose(num);
+			}
+		} else {
+			if (left == null) {
+				return false;
+			} else {
+				return left.containsVerbose(num);
+			}
+		}
+	}
+	
+}
